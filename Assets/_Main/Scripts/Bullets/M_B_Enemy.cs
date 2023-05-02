c8cb395bfaa77404b13151e4a8d37d207ac2be48
Jahaha1 <ab2805281@gmail.com>
2023-05-02 16:54:02 +0800

    Effect Added & Before Slow Motion

---
+++ Assets/_Main/Scripts/Bullets/M_B_Enemy.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Feedbacks;
+
+public class M_B_Enemy : M_Bullet
+{
+    public MMF_Player mmf_HitByPlayer;
+
+    void Start()
+    {
+        EnvironmentHitted += mmf_EnvironmentHitted.PlayFeedbacks;
+        Initialize_Bullet();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = direction * moveSpeed;
+        DrawPredictionTrajectory();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        OnHitWallReflectBullet(collision);
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.rotation = Quaternion.Euler(90, transform.rotation.y, 0);
+            direction = Vector3.up;
+            moveSpeed = 5;
+            GetComponent<CapsuleCollider>().isTrigger = true;
+            mmf_HitByPlayer.PlayFeedbacks();
+        }
+    }
+
+    private void DrawPredictionTrajectory()
+    {
+        List<Vector3> linePositions = GetHitPoints();
+
+        lr.positionCount = linePositions.Count;
+        for (int i = 0; i < linePositions.Count; i++)
+        {
+            lr.SetPosition(i, linePositions[i]);
+        }
+    }
+
+    private List<Vector3> GetHitPoints()
+    {
+        RaycastHit hit;
+        int hitPointCount = 0;
+        Vector3 start = transform.position;
+        Vector3 direction = transform.TransformDirection(Vector3.forward);
+        List<Vector3> linePositions = new List<Vector3>();
+        linePositions.Add(start);
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (Physics.Raycast(start, direction, out hit, Mathf.Infinity, 1 << layer_Environment | 1 << LayerMask.NameToLayer("Bound")))
+                if (hit.collider.gameObject.layer == layer_Environment)
+                {
+                    linePositions.Add(hit.point);
+                    start = hit.point;
+                    direction = Vector3.Reflect(direction, hit.normal).normalized;
+                    direction = new Vector3(direction.x, 0, direction.z);
+                    hitPointCount++;
+                }
+                else
+                {
+                    linePositions.Add(hit.point);
+                    break;
+                }
+        }
+        return linePositions;
+    }
+}
---
+++ Assets/_Main/Scripts/Bullets/M_Bullet.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using MoreMountains.Feedbacks;
+
+public class M_Bullet : MonoBehaviour
+{
+    public float moveSpeed;
+    protected Rigidbody rb;
+    protected Vector3 direction;
+    protected LayerMask layer_Environment;
+    public LineRenderer lr;
+
+    protected Action EnvironmentHitted;
+    public MMF_Player mmf_EnvironmentHitted;
+
+    private void Start()
+    {
+   
+    }
+
+    public void Initialize_Bullet()
+    {
+        //设置初始射击方向
+        direction = transform.forward;
+        //添加刚体组件
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        //设置碰撞的环境Layer
+        layer_Environment = LayerMask.NameToLayer("Environment");
+    }
+
+    public void OnHitWallReflectBullet(Collision collision)
+    {
+        //当与Layer为Environment的物体碰撞时，弹射自身
+        if (collision.gameObject.layer == layer_Environment)
+        {
+            rb.velocity = Vector3.zero;
+            direction = Vector3.Reflect(direction, collision.contacts[0].normal).normalized;
+            direction = new Vector3(direction.x, 0, direction.z);
+            transform.forward = direction;
+            EnvironmentHitted();
+        }
+    }
+
+    protected void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.red);
+    }
+}
